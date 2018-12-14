0b1007f11f07e101b244da6a18e3891fad899080
AlexandrEmma <hublaualexandre@gmail.com>
2018-12-14 15:51:41 +0100

    Ajout patrons des methodes dans la classe

--- src/ffmpeg_tools/SystemRequests.java
+++ src/ffmpeg_tools/SystemRequests.java
@@ -1,5 +1,7 @@
 package ffmpeg_tools;
 
+import java.io.File;
+
 public class SystemRequests {
 
 	/**
@@ -26,5 +28,96 @@ public class SystemRequests {
 	 * TODO
 	 */
 	public final int[] VIDEO_RESOLUTION = new int[] {};
+
+	/**
+	 * TODO
+	 */
+	public SystemRequests() {
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param file
+	 * @param l
+	 * @return
+	 */
+	public File createVideoFrame(File file, long l) {
+		return file;
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param file
+	 * @param tab
+	 * @return
+	 */
+	public File createThumbail(File file, int[] tab) {
+		return file;
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param file
+	 * @param l
+	 * @return
+	 */
+	private File extractImage(File file, long l) {
+		return file;
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param file
+	 * @param tab
+	 * @return
+	 */
+	private int[] resizeImage(File file, int[] tab) {
+		return tab;
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param file
+	 * @param tab
+	 * @return
+	 */
+	private int[] findGoodResolution(File file, int[] tab) {
+		return tab;
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param file
+	 * @param l
+	 * @return
+	 */
+	public long getDuration(File file, long l) {
+		return l;
+	}
+
+	/**
+	 * TODO
+	 * @param file
+	 * @param s
+	 * @return
+	 */
+	public long getDuration(File file, String s) {
+		return 0;
+	}
+
+	/**
+	 * TODO
+	 * @param file
+	 * @return
+	 */
+	public Object[] getSettings(File file) {
+		return null;
+	}
 
 }

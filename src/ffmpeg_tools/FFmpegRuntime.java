a3453ce80da6a040a9022c5583b574564f0ac128
chevrier jc <chevrie44u@etu.univ-lorraine.fr>
2018-12-14 15:54:49 +0100

    Mise a jour.

---
+++ src/ffmpeg_tools/FFmpegRuntime.java
@@ -0,0 +1,23 @@
+package ffmpeg_tools;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class FFmpegRuntime {
+	protected static String PATH_FFMPEG;
+	protected final static Runtime RUN = Runtime.getRuntime();
+	
+	private void install() throws IOException {
+		if(System.getProperty("os.name").contains("Linux")) {
+			RUN.exec("chmod u+x ../../scripts/intall_ffmpeg_linux.sh && ../../scripts/intall_ffmpeg_linux.sh");
+			PATH_FFMPEG = "ffmpeg";
+		}else
+			PATH_FFMPEG = "../../ffmpeg/bin/ffmpeg.exe";
+	}
+	
+	public void execute(String ffmpegRequest) throws IOException {
+		if(PATH_FFMPEG==null) this.install();
+		RUN.exec(PATH_FFMPEG+ffmpegRequest);
+	}
+		
+}
--- src/ffmpeg_tools/FfmpegRunTime.java
+++
@@ -1,9 +0,0 @@
-package ffmpeg_tools;
-
-public abstract class FfmpegRunTime {
-
-	public static void main(String[] args) {
-		System.out.println(System.getProperty("os.name"));
-	}
-
-}

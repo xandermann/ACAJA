85cd8f0969ff6a606c1966c7031f11e87dbd0233
AlexandrEmma <hublaualexandre@gmail.com>
2018-12-14 15:46:18 +0100

    Ajout constantes

---
+++ src/ffmpeg_tools/SystemRequests.java
@@ -0,0 +1,30 @@
+package ffmpeg_tools;
+
+public class SystemRequests {
+
+	/**
+	 * TODO
+	 */
+	public final String PATH_TEMPORARY_FILES = "";
+
+	/**
+	 * TODO
+	 */
+	public final int[] LIBRARY_THUMBAIL_RESOLUTION = new int[] {};
+
+	/**
+	 * TODO
+	 */
+	public final int[] VIDEO_TIMELINE_THUMBAIL_RESOLUTION = new int[] {};
+
+	/**
+	 * 
+	 */
+	public final int[] SOUND_TIMELINE_THUMBAIL_RESOLUTION = new int[] {};
+
+	/**
+	 * TODO
+	 */
+	public final int[] VIDEO_RESOLUTION = new int[] {};
+
+}

49e20ab5ebf3459922c7308b887901d589b87882
AlexandrEmma <hublaualexandre@gmail.com>
2018-12-17 14:32:14 +0100

    Ajout javadoc SelectableFile

--- src/files/SelectableFile.java
+++ src/files/SelectableFile.java
@@ -4,18 +4,36 @@ import java.io.File;
 
 public class SelectableFile {
 
+	/**
+	 * Fichier source
+	 */
 	protected File sourceFile;
 
+	/**
+	 * Vrai s'il est selectionne
+	 */
 	private boolean isSelected;
 
+	/**
+	 * Definition a partir du fichier source
+	 * @param file Fichier source
+	 */
 	public void SelectionFile(File file) {
 		this.sourceFile = file;
 	}
 	
+	/**
+	 * Test si le fichier est selectionne
+	 * @return Vrai s'il est selectionne
+	 */
 	public boolean isSelected() {
 		return isSelected;
 	}
 	
+	/**
+	 * Test si le fichier est selectionne
+	 * @param isSelected
+	 */
 	public void setSelected(boolean isSelected) {
 		this.isSelected = isSelected;
 	}

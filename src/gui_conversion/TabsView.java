e7a4422ae7a4e28b07f0d97149fc69211ed5cb3b
dasilvac9u <alexandre.da-silva-carmo9@etu.univ-lorraine.fr>
2018-12-17 14:57:15 +0100

    squellette vue

--- src/gui_conversion/TabsView.java
+++ src/gui_conversion/TabsView.java
@@ -1,5 +1,22 @@
 package gui_conversion;
 
-public class TabsView {
+import java.util.Observable;
+import java.util.Observer;
 
+public class TabsView implements Observer{
+	
+	public ConversionModel model;
+	
+	public TabsView(ConversionModel m) {
+		this.model = m;
+	}
+	
+	private void reevaluateTabbedPanel() {
+		
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		
+	}
 }

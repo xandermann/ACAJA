5a2558bcbdd181d6e5ec81f8df86a14c51f510c8
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2018-12-17 15:42:54 +0100

    Ajout contenu methodes ConversionModel

--- src/gui_conversion/ConversionModel.java
+++ src/gui_conversion/ConversionModel.java
@@ -3,16 +3,22 @@ package gui_conversion;
 import java.io.*;
 import java.util.*;
 import files.*;
+import javax.swing.*;
 
 public class ConversionModel {
 
 	private SettingsFile currentFile;
 	private ArrayList<SettingsFile> files;
+	// nouvel attribut rajoute pour les profils charges
+	private ArrayList<Profile> profiles;
 	
 	/** Constructeur ConversionModel
 	 * 
 	 */
-	public ConversionModel() {	}
+	public ConversionModel() {
+		this.files = new ArrayList<SettingsFile>();
+		this.profiles = new ArrayList<Profile>();
+	}
 	
 	/*Les profils seront enregistres dans le dossier du programme
 	 *et seront tous charges au demarrage. L'utilisateur pourra gerer les profils
@@ -24,22 +30,40 @@ public class ConversionModel {
 	 * 
 	 */
 	// a modifier public void loadProfiles() {}
-	public void loadProfile(File profileFile) { }
+	// public void loadProfile(File profileFile) { }
+	public void loadProfiles() {
+		// Charger les fichiers profils dans le dossier des profils
+		// Creer des objets de type profils en lisant les fichiers*
+		// Ajouter les fichiers dans la liste
+	}
 	/**
 	 * Methode qui enregistre un nouveau profil parmi les profils
 	 * 
 	 * @param profile Profile : nouveau profil a enregistrer
 	 */
 	// a modifier public void saveProfile(Profile profile) {}
-	public void saveProfile(String profile) { }
+	// anciennement public void saveProfile(String profile) { }
+	public void saveProfile(Profile profile) {
+		// Creer un nouveau fichier contenant les parametres du profils
+		// Enregistrer le fichier dans le dossier des profils
+		
+	}
+	
 	/**
 	 * Methode qui supprime un des profils existants
 	 * 
 	 * @param profile Profile : profil a supprimer
 	 */
 	// a modifier public void removeProfile(Profile profile) {}
-	public void removeProfile(File profileFile) { }
-
+	//anciennement public void removeProfile(File profileFile) { }
+	public void removeProfile(Profile profile) {
+		if(this.profiles.contains(profile)) {
+			this.profiles.remove(profile);
+		}  else {
+			JOptionPane.showMessageDialog(null, "Le profil a supprimer n'existe pas");
+		}
+	}
+	
 	/**
 	 * Methode qui sauvegarde les fichiers recemments ouverts a la fermeture
 	 */
@@ -70,29 +94,51 @@ public class ConversionModel {
 	 * Methode qui ajoute un fichier a la bibliotheque
 	 * @param file SettingsFile : fichier a ajouter a la bibliotheque
 	 */
-	public void add(SettingsFile file) { }
+	public void add(File file) {
+		if(file.exists()) {
+			this.files.add(new SettingsFile(file));
+		} else {
+			JOptionPane.showMessageDialog(null, "Le fichier selectionne n'existe pas");
+		}
+	}
 	
 	/**
 	 * Methode qui supprime un fichier de la bibliotheque
 	 * @param file SettingsFile ! fichier a supprimer de la bibliotheque
 	 */
-	public void remove(SettingsFile file) {	}
+	public void remove(SettingsFile file) {	
+		if(this.files.contains(file)) {
+			this.files.remove(file);
+		} else {
+			JOptionPane.showMessageDialog(null, "Le fichier a supprimer n'est pas present dans la bibliotheque");
+		}
+	}
 	
 	
 	/**
 	 * Methode qui demarre la conversion des SettingsFile modifies
 	 */
-	public void convert() { }
+	public void convert() {
+		for(SettingsFile sf : this.files) {
+			if(sf.isModified()) {
+				// Convertir
+			}
+		}
+	}
 	
 	/**
 	 * Methode qui vide la bibliotheque
 	 */
-	public void clear() { }
+	public void clear() { 
+		this.files.clear();
+	}
 	
 	/**
 	 * Methode qui permet de modifier un reglage du fichier actuellement selectionne
 	 */
-	public void modify(String parametre, Object valeur) { }
+	public void modify(String settings, Object value) { 
+		this.currentFile.modifySettings(settings, value);
+	}
 
 	
 	/**
---
+++ src/gui_conversion/Profile.java
@@ -0,0 +1,5 @@
+package gui_conversion;
+
+public class Profile {
+
+}

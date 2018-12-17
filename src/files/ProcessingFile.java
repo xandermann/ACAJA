f3b42afdb1c701935e3f2cf3e90a96df1fdc17d8
AlexandrEmma <hublaualexandre@gmail.com>
2018-12-17 14:58:06 +0100

    Ajout class ProcessingFile

---
+++ src/files/ProcessingFile.java
@@ -0,0 +1,28 @@
+package files;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class ProcessingFile {
+
+	private long duration;
+
+	private HashMap<String, Object> performedProcessings;
+
+	public ProcessingFile(File file) {
+		// TODO
+	}
+
+	public void performProcess(String string, Object object) {
+		// TODO
+	}
+	
+	public HashMap<String, Object> getProcessings(){
+		// TODO
+	}
+	
+	public HashMap<String, Object> getPerformedProcessings(){
+		return this.performedProcessings;
+	}
+
+}

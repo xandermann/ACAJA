733a7bc1f01d0930fabaa358d492346c09b2068e
chevrier jc <chevrie44u@etu.univ-lorraine.fr>
2018-12-18 08:28:46 +0100

    Correction minime de OpeningWindow.

--- src/main/OpeningWindow.java
+++ src/main/OpeningWindow.java
@@ -6,45 +6,39 @@ import java.awt.*;
 import javax.swing.*;
 
 public class OpeningWindow {
-	private static int x = 750;
-	private static int y = 500;
-	public OpeningWindow() {
-		
-	}
+	private final static int WIDTH = 750;
+	private final static int HEIGHT = 500;
 	
-	private static JFrame generateLoardingWindow() {
+	private static void generateLoardingWindow() {
 		JFrame frame = new JFrame("Chargement");
 		frame.setLocation(100, 100);
-		frame.setSize(new Dimension(x,y));
+		frame.setSize(new Dimension(WIDTH,HEIGHT));
 		//frame.pack();
 		frame.setVisible(true);
 		try {
-			Thread.sleep(1500);
+			Thread.sleep(2500);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		frame.dispose();
-		return frame;
 	}
 	
-	private static JFrame generateChoiceModeWindow() {
-		JFrame frame = new JFrame("Chargement");
-		frame.setLocation(100, 100);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(new Dimension(x,y));
-		frame.setLayout(new GridLayout(2,1));
-		
+	private static void generateChoiceModeWindow() {
 		JButton j1 = new JButton("Conversion");
 		j1.setPreferredSize(new Dimension(100, 50));
 		JButton j2 = new JButton("Traitement");
 		j2.setPreferredSize(new Dimension(100, 50));
 		
+		JFrame frame = new JFrame("Chargement");
+		
 		frame.add(j1);
 		frame.add(j2);
 		
+		frame.setLocation(100, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(new Dimension(WIDTH,HEIGHT));
+		frame.setLayout(new GridLayout(2,1));
 		frame.setVisible(true);
-		
-		return frame;
 	}
 	
 	public static void generateOpeningWindow() {

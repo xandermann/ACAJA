408fbbbeeaa6fc00b6289829e052059c8f9eccfc
AlexandrEmma <hublaualexandre@gmail.com>
2019-01-10 14:51:00 +0100

    Clef VALEUR OK

--- src/files/SettingsFile.java
+++ src/files/SettingsFile.java
@@ -15,6 +15,82 @@ import ffmpeg_tools.SystemRequests;
  *         CHEVRIER Jean-christophe.
  */
 public class SettingsFile extends SelectableFile {
+	
+	/*============================================================*/
+
+	/**
+	 * [ CONSTANTES DE CLASSES A USAGE INTERNE. ]
+	 * 
+	 * Voici des constantes qui nous seront bien pratiques. Pour la majorite, ce
+	 * sont des contantes representant des types de resolutions ( par exemple :
+	 * resolution d'une frame de l'apercu de lecture de video du logiciel ).
+	 * 
+	 * Ces constantes ne seront destines a etre utilises que pour le fonctionnement
+	 * interne de la fenetre de traitement.
+	 * 
+	 * Elles sont destinees a un usage uniquement interne ( d'ou l presence des
+	 * private ).
+	 */
+
+	/**
+	 * [ CHEMIN VERS LE REPERTOIRE DES FICHIERS TEMPORAIRES. ]
+	 * 
+	 * TODO iteration 1 pas concernee.
+	 * 
+	 * Ceci est le chemin vers le repertoire des fichiers temporaires. Ces fameux
+	 * ficheirs temporaires seront en fait les miniatures de video, et les frames de
+	 * d'apercu de lecture de video, etc.
+	 */
+	private final static String PATH_TEMPORARY_FILES = System.getProperty("user.dir") + "\\temporary_files\\";
+
+	/**
+	 * [ RESOLUTION DES MINIATURES DANS LES BIBLIOTHEQUES. ]
+	 * 
+	 * TODO iteration 1 pas concernee.
+	 * 
+	 * Ceci est le tableau representant la resolution des miniatutres des videos,
+	 * des sons, et des images qui seront importes par l'utilisateur dans le
+	 * logiciel.
+	 */
+	private final static int[] LIBRARY_THUMBAIL_RESOLUTION = new int[] {};
+
+	/**
+	 * [ RESOLUTION DES MINIATURES DES VIDEO DANS LA CHRONOLOGIE VIDEO. ]
+	 * 
+	 * TODO iteration 1 pas concernee.
+	 * 
+	 * Ceci est le tableau representant la resolution des miniatutres des videos,
+	 * dans la chronologie video.
+	 */
+	private final static int[] VIDEO_TIMELINE_THUMBAIL_RESOLUTION = new int[] {};
+
+	/**
+	 * [ RESOLUTION DES MINIATURES DES SONS DANS LES CHRONOLOGIES DES SONS. ]
+	 * 
+	 * TODO iteration 1 pas concernee.
+	 * 
+	 * Ceci est le tableau representant la resolution des miniatutres des sons, dans
+	 * les chronologies des sons.
+	 */
+	private final static int[] SOUND_TIMELINE_THUMBAIL_RESOLUTION = new int[] {};
+
+	/**
+	 * [ RESOLUTION DES FRAMES DANS L'APERCU DE LECTURE DE VIDEO. ]
+	 * 
+	 * TODO iteration 1 pas concernee.
+	 * 
+	 * Ceci est le tableau representant la resolution frames dans l'apercu de
+	 * lecture de video de la fenetre de traitement.
+	 */
+	private final static int[] VIDEO_RESOLUTION = new int[] {};
+
+	/*============================================================*/
+	
+	/**
+	 * Constante ajoutée: clé de la hashmap
+	 */
+	public final static String KEY_FORMAT = "FORMAT";
+	
 
 	/**
 	 * Les anciens parametres du fichier
@@ -53,7 +129,7 @@ public class SettingsFile extends SelectableFile {
 		settings = new HashMap<String, Object>();
 
 		// Initialisation des autres parametres.
-		//SystemRequests.getSettings(this);
+		// SystemRequests.getSettings(this);
 
 	}
 
@@ -66,7 +142,6 @@ public class SettingsFile extends SelectableFile {
 	public void modifySettings(String setting, String newValue) {
 		this.oldSettings.put(setting, this.settings.get(setting));
 		this.settings.put(setting, newValue);
-
 	}
 
 	/**
--- src/gui_conversion/VideoSettingsPanel.java
+++ src/gui_conversion/VideoSettingsPanel.java
@@ -1,10 +1,10 @@
 package gui_conversion;
 
-import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import java.io.File;
 
 import javax.swing.BoxLayout;
@@ -15,68 +15,77 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class VideoSettingsPanel extends JPanel{
-	
+public class VideoSettingsPanel extends JPanel {
+
 	private ConversionModel model;
-	
+
 	public VideoSettingsPanel(ConversionModel m) {
 		this.model = m;
 		this.reevaluatePanel();
 	}
-	
+
 	private void reevaluatePanel() {
 		this.setSize(new Dimension(400, 400));
-		
+
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
+
 		JPanel Codec = new JPanel();
 		Codec.setLayout(new FlowLayout());
-		
+
 		/*
 		 * NOTE : pour la premiere iteration nous avons choisi d'implementer simplement
-		 * un FORMAT de sortie, par la suite, l'utilisateur choisira les codecs audio/videos voulu
-		 * et le conteneur(format) sera determine automatiquement par le logiciel pour creer un fichier
-		 * optimal et lisible
+		 * un FORMAT de sortie, par la suite, l'utilisateur choisira les codecs
+		 * audio/videos voulu et le conteneur(format) sera determine automatiquement par
+		 * le logiciel pour creer un fichier optimal et lisible
 		 */
-		Codec.add(new JLabel("Format sortie : "),BorderLayout.WEST);
-		String[] format = {"avi", "mp4", "flv"};
+		Codec.add(new JLabel("Format sortie : "), BorderLayout.WEST);
+		String[] format = { "avi", "mp4", "flv" };
 		JComboBox box_format = new JComboBox(format);
 		
-		Codec.add(box_format,BorderLayout.EAST);
-		
+		box_format.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(model.getCurrentFile() != null) {
+					model.getCurrentFile().modifySettings(model.getCurrentFile().KEY_FORMAT, ((JComboBox)e.getSource()).getSelectedItem().toString());
+				}
+			}
+		});
+
+		Codec.add(box_format, BorderLayout.EAST);
+
 		JPanel reso = new JPanel();
 		reso.setLayout(new FlowLayout());
-		reso.add(new JLabel("Resolution : "),BorderLayout.WEST);
+		reso.add(new JLabel("Resolution : "), BorderLayout.WEST);
 		JTextField resolution_Text = new JTextField();
-		resolution_Text.setPreferredSize(new Dimension(300,20));
+		resolution_Text.setPreferredSize(new Dimension(300, 20));
 		resolution_Text.setEnabled(false);
-		reso.add(resolution_Text,BorderLayout.EAST);
-		
+		reso.add(resolution_Text, BorderLayout.EAST);
+
 		JPanel br = new JPanel();
 		br.setLayout(new FlowLayout());
-		br.add(new JLabel("Bitrate (kb/s) : "),BorderLayout.WEST);
+		br.add(new JLabel("Bitrate (kb/s) : "), BorderLayout.WEST);
 		JTextField bitrate_Text = new JTextField();
-		bitrate_Text.setPreferredSize(new Dimension(300,20));
+		bitrate_Text.setPreferredSize(new Dimension(300, 20));
 		bitrate_Text.setEnabled(false);
-		br.add(bitrate_Text,BorderLayout.EAST);
-		
+		br.add(bitrate_Text, BorderLayout.EAST);
+
 		JPanel panef = new JPanel();
 		panef.setLayout(new FlowLayout());
-		panef.add(new JLabel("Images par seconde (FPS) : "),BorderLayout.WEST);
+		panef.add(new JLabel("Images par seconde (FPS) : "), BorderLayout.WEST);
 		JTextField fps_Text = new JTextField();
-		fps_Text.setPreferredSize(new Dimension(300,20));
+		fps_Text.setPreferredSize(new Dimension(300, 20));
 		fps_Text.setEnabled(false);
-		panef.add(fps_Text,BorderLayout.EAST);
-		
+		panef.add(fps_Text, BorderLayout.EAST);
+
 		JPanel panest = new JPanel();
 		panest.setLayout(new FlowLayout());
-		panest.add(new JLabel("Fichiers sous-titres : "),BorderLayout.WEST);
-		
-		
+		panest.add(new JLabel("Fichiers sous-titres : "), BorderLayout.WEST);
+
 		JButton parcourir = new JButton("Parcourir");
 		parcourir.setEnabled(false);
-		parcourir.setPreferredSize(new Dimension(100,20));
-		parcourir.addActionListener(new ActionListener(){
+		parcourir.setPreferredSize(new Dimension(100, 20));
+		parcourir.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// ATTENTION
@@ -89,16 +98,14 @@ public class VideoSettingsPanel extends JPanel{
 				// Ce fichier sera le fichier des sous-titres, pas un SettingsFile !
 			}
 		});
-		panest.add(parcourir,BorderLayout.EAST);
-		
-		
-		
+		panest.add(parcourir, BorderLayout.EAST);
+
 		this.add(Codec);
 		this.add(reso);
 		this.add(br);
 		this.add(panef);
 		this.add(panest);
-		//this.repaint();
+		// this.repaint();
 	}
-	
+
 }

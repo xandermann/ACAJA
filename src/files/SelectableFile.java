c260d560170ff86ff8424c7f844746b5cf4a4491
AlexandrEmma <hublaualexandre@gmail.com>
2019-01-10 13:57:40 +0100

    Commentaires

--- src/files/SelectableFile.java
+++ src/files/SelectableFile.java
@@ -1,47 +1,46 @@
 package files;
+
 import java.io.File;
+
 /**
- * TODO comentaire a faire. 
+ * TODO comentaire a faire.
  * 
- * Auteurs du projet : 
- * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et CHEVRIER Jean-christophe.
+ * Auteurs du projet :
+ * 
+ * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et
+ *         CHEVRIER Jean-christophe.
  */
 public class SelectableFile {
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
+
 	/**
 	 * [ CONSTANTES DE CLASSE. ]
 	 * 
-	 * Ces constantes permettent d'indiquer le type du
-	 * fichier source d'une instance de la classe. 
+	 * Ces constantes permettent d'indiquer le type du fichier source d'une instance
+	 * de la classe.
 	 */
-	
+
 	/**
-	 * Constante indiquant que le fichier est un fichier video. 
+	 * Constante indiquant que le fichier est un fichier video.
 	 */
 	public final static int FILE_TYPE_VIDEO = 0;
 
 	/**
-	 * Constante indiquant que le fichier est un fichier audio.  
+	 * Constante indiquant que le fichier est un fichier audio.
 	 */
 	public final static int FILE_TYPE_AUDIO = 1;
 
 	/**
-	 * Constante indiquant que le fichier est un fichier image. 
+	 * Constante indiquant que le fichier est un fichier image.
 	 */
 	public final static int FILE_TYPE_IMAGE = 2;
 
-	
 	/**
-	 * [ ATTRIBUT D'INSTANCE. ] 
+	 * [ ATTRIBUT D'INSTANCE. ]
 	 */
-	
+
 	/**
-	 * Type du fichier source. Il ne peut prendre que 3 valeurs : 
-	 * - FILE_TYPE_VIDEO ( = 0) ;
-	 * - FILE_TYPE_AUDIO ( = 1) ; 
-	 * - FILE_TYPE_IMAGE ( = 2).
+	 * Type du fichier source. Il ne peut prendre que 3 valeurs : - FILE_TYPE_VIDEO
+	 * ( = 0) ; - FILE_TYPE_AUDIO ( = 1) ; - FILE_TYPE_IMAGE ( = 2).
 	 */
 	protected int typeFile;
 
@@ -51,155 +50,113 @@ public class SelectableFile {
 	protected File sourceFile;
 
 	/**
-	 * Cet attribut permet d'indiquer si this est selectionne. 
+	 * Cet attribut permet d'indiquer si this est selectionne.
 	 */
 	private boolean isSelected;
 
-
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-
-	
 	/**
 	 * [ CONSTRUCTEUR. ]
 	 * 
-	 * Ce constructeur intialise les attributs. 
+	 * Ce constructeur intialise les attributs.
 	 * 
-	 * @param file 		Le fichier source. 
+	 * @param file Le fichier source.
 	 */
 	public SelectableFile(File file) {
-		if(file == null)
+		if (file == null)
 			throw new NullPointerException("Erreur : fichier fourni en parametre null !");
 		this.isSelected = false;
 		this.sourceFile = file;
 		this.whoAmI();
 	}
-	
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
-	
+
 	/**
 	 * [ METHODE - ROLE PRIMITIF. ]
 	 * 
-	 * Cette methode a un role primitif, elle permet
-	 * de verifier si le fichier source est une video. 
+	 * Cette methode a un role primitif, elle permet de verifier si le fichier
+	 * source est une video.
 	 * 
-	 * @return booleen		Vaut true si le fichier source 
-	 * 						est une video. 
+	 * @return booleen Vaut true si le fichier source est une video.
 	 */
 	public boolean isVideo() {
-		return typeFile==SelectableFile.FILE_TYPE_VIDEO;
+		return typeFile == SelectableFile.FILE_TYPE_VIDEO;
 	}
-	
+
 	/**
-	 *  [ METHODE - ROLE PRIMITIF. ]
-	 *  
-	 *  Cette methode a un role primitif, elle permet 
-	 *  de verifier si le fichier source est un son.
-	 *  
-	 * @return booleen		Vaut true si le fichier source 
-	 * 						est un son. 
+	 * [ METHODE - ROLE PRIMITIF. ]
+	 * 
+	 * Cette methode a un role primitif, elle permet de verifier si le fichier
+	 * source est un son.
+	 * 
+	 * @return booleen Vaut true si le fichier source est un son.
 	 */
 	public boolean isSound() {
-		return typeFile==SelectableFile.FILE_TYPE_AUDIO;
+		return typeFile == SelectableFile.FILE_TYPE_AUDIO;
 	}
-	
-	
-	
+
 	/**
-	 *  [ METHODE - ROLE PRIMITIF. ]
-	 *  
-	 *  Cette methode a un role primitif, elle permet
-	 *  se verifier si le fichier source est un son. 
-	 *  
-	 * @return booleen 		Vaut true si le fichier source 
-	 * 						est une video ou un son. 
+	 * [ METHODE - ROLE PRIMITIF. ]
+	 * 
+	 * Cette methode a un role primitif, elle permet se verifier si le fichier
+	 * source est un son.
+	 * 
+	 * @return booleen Vaut true si le fichier source est une video ou un son.
 	 */
 	public boolean isGoodFile() {
 		System.out.println(isVideo());
 		System.out.println(isSound());
-		System.out.println(isVideo() || isSound() );
-		return ( isVideo() || isSound() );
+		System.out.println(isVideo() || isSound());
+		return (isVideo() || isSound());
 	}
-	
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
-	
-	/** 
+
+	/**
 	 * [ METHODE POUR RENSEIGNER LE TYPE DE FICHIER. ]
 	 * 
-	 * Cette methode permet de savoir si le fichier 
-	 * source est une video, ou un son, ou meme une image.
+	 * Cette methode permet de savoir si le fichier source est une video, ou un son,
+	 * ou meme une image.
 	 * 
-	 * 3 valeurs possiblement accordable a typeFile : 
-	 * - FILE_TYPE_VIDEO ( = 0 ) ;
-	 * - FILE_TYPE_AUDIO ( = 1 ) ;
-	 * - FILE_TYPE_IMAGE ( = 2 ).
+	 * 3 valeurs possiblement accordable a typeFile : - FILE_TYPE_VIDEO ( = 0 ) ; -
+	 * FILE_TYPE_AUDIO ( = 1 ) ; - FILE_TYPE_IMAGE ( = 2 ).
 	 */
 	private void whoAmI() {
-		
+
 		String fileName = this.sourceFile.getName().toLowerCase();
 		System.out.println(fileName);
 		/**
-		 * TODO ajouter des extensions de video possibles. 
+		 * TODO ajouter des extensions de video possibles.
 		 */
-		
-		if(fileName.endsWith("mp4") || fileName.endsWith("avi")) 
-			typeFile = FILE_TYPE_VIDEO; 
-		else if(fileName.endsWith("mp3") || fileName.endsWith("wav") || fileName.endsWith("ogg"))
-				typeFile = FILE_TYPE_AUDIO;
-			 else if(fileName.endsWith("png") || fileName.endsWith("jpg") || fileName.endsWith("jpeg")) 
-				 	typeFile = FILE_TYPE_IMAGE;
+
+		if (fileName.endsWith("mp4") || fileName.endsWith("avi"))
+			typeFile = FILE_TYPE_VIDEO;
+		else if (fileName.endsWith("mp3") || fileName.endsWith("wav") || fileName.endsWith("ogg"))
+			typeFile = FILE_TYPE_AUDIO;
+		else if (fileName.endsWith("png") || fileName.endsWith("jpg") || fileName.endsWith("jpeg"))
+			typeFile = FILE_TYPE_IMAGE;
 	}
 
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
-	
 	/**
 	 * [ METHODE POUR SAVOIR SI THIS A ETE MODIFIE. ]
 	 * 
-	 * Methode abstraite dont le comportement en interne change selon si on est 
-	 * dans la classe fille SettingsFile ou dans la classe fille ProcessingFile.
+	 * Methode abstraite dont le comportement en interne change selon si on est dans
+	 * la classe fille SettingsFile ou dans la classe fille ProcessingFile.
 	 * 
-	 * En effet on dans quelle fille, etre modifiee a un sens different. 
-	 * Cela justifie  la declaration en abstract de la methode. 
+	 * En effet on dans quelle fille, etre modifiee a un sens different. Cela
+	 * justifie la declaration en abstract de la methode.
 	 * 
-	 * Cette methode sera tres utile au moment ou l'utilisateur du logiciel
-	 * voudra generer le fichier de sortie, elle permettra des lors 
-	 * de savoir si this a bien ete modifie et est bien a prendre en compte
-	 * lors de la generation du fichier de sortie. 
+	 * Cette methode sera tres utile au moment ou l'utilisateur du logiciel voudra
+	 * generer le fichier de sortie, elle permettra des lors de savoir si this a
+	 * bien ete modifie et est bien a prendre en compte lors de la generation du
+	 * fichier de sortie.
 	 * 
-	 * @return booleen		Vaut true si this a ete modifie. 
-	 * public abstract boolean isModified();
+	 * @return booleen Vaut true si this a ete modifie. public abstract boolean
+	 *         isModified();
 	 */
-	
-	
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
+
 	/**
 	 * [ METHODE ACCESSEUR - GETTER. ]
 	 * 
-	 * Methode pour tester si this est selectionne. 
+	 * Methode pour tester si this est selectionne.
 	 * 
-	 * @return booleen		True si this est selectionne. 
+	 * @return booleen True si this est selectionne.
 	 */
 	public boolean isSelected() {
 		return isSelected;
@@ -208,7 +165,7 @@ public class SelectableFile {
 	/**
 	 * [ METHODE ACCESSEUR - SELECTIONNER THIS - SETTER. ]
 	 * 
-	 * Methode pour selectionner this. 
+	 * Methode pour selectionner this.
 	 */
 	public void select() {
 		isSelected = true;
@@ -217,7 +174,7 @@ public class SelectableFile {
 	/**
 	 * [ METHODE ACCESSEUR - DESELECTIONNER THIS - SETTER. ]
 	 * 
-	 * Methode pour deselectionner this. 
+	 * Methode pour deselectionner this.
 	 */
 	public void deselect() {
 		isSelected = false;
@@ -226,33 +183,32 @@ public class SelectableFile {
 	/**
 	 * [ METHODE ACCESSEUR - GETTER. ]
 	 * 
-	 * Methode pour recuperer le type du fichier. 
+	 * Methode pour recuperer le type du fichier.
 	 * 
-	 * @return int			Le type du fichier. 
+	 * @return int Le type du fichier.
 	 */
 	public int getTypeFile() {
 		return this.typeFile;
 	}
-	
+
 	/**
 	 * [ METHODE ACCESSEUR - GETTER. ]
 	 * 
-	 * Methode pour recuperer le fichier source. 
+	 * Methode pour recuperer le fichier source.
 	 * 
-	 * @return File		Le fichier source.
+	 * @return File Le fichier source.
 	 */
 	public File getSourceFile() {
 		return sourceFile;
 	}
-	
-	/** Methode pour recuperer le nom du fichier
+
+	/**
+	 * Methode pour recuperer le nom du fichier
+	 * 
 	 * @return String Le nom du fichier source
 	 */
 	public String getSourceFilename() {
 		return sourceFile.getName();
 	}
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
+
 }
--- src/files/SettingsFile.java
+++ src/files/SettingsFile.java
@@ -1,23 +1,20 @@
 package files;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import ffmpeg_tools.SystemRequests;
+
 /**
- * TODO comentaire a faire. 
+ * TODO comentaire a faire.
  * 
- * Auteurs du projet : 
- * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et CHEVRIER Jean-christophe.
+ * Auteurs du projet :
+ * 
+ * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et
+ *         CHEVRIER Jean-christophe.
  */
 public class SettingsFile extends SelectableFile {
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
-	
-	/**
-	 * [ ATTRIBUTS. ]
-	 */
+
 	/**
 	 * Les anciens parametres du fichier
 	 */
@@ -28,75 +25,53 @@ public class SettingsFile extends SelectableFile {
 	 */
 	private HashMap<String, Object> settings;
 
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
-	
 	/**
-	 * [ CONSTRUCTEUR. ]
+	 * Constructeur
 	 * 
-	 * @param file   	 				Le fichier source. 
-	 *
-	 * @throws IOException 
-	 * @throws InterruptedException 	
+	 * @param file Le fichier source.
+	 * @throws IOException
+	 * @throws InterruptedException
 	 */
-	public SettingsFile(File file){
+	public SettingsFile(File file) {
 		/**
 		 * INITIALISATION DES ATTRIBUTS HETITES DE LA CLASSE SELECTABLEFILE.
 		 */
 		super(file);
-		
+
 		/**
 		 * SI TYPE DE FICHIER PAS ACCCPTE EXCPETION.
 		 */
-		if(!isGoodFile())
-			throw new IllegalArgumentException("Seuls les fichiers audio et video sont toleres."); 
+		if (!isGoodFile())
+			throw new IllegalArgumentException("Seuls les fichiers audio et video sont toleres.");
 
 		/**
-		 * INITIALISATION DES PARAMETRES DE LA VIDEO. 
+		 * INITIALISATION DES PARAMETRES DE LA VIDEO.
 		 */
-		//Intiliasation des tables. 
+		// Intiliasation des tables.
 		oldSettings = new HashMap<String, Object>();
 		settings = new HashMap<String, Object>();
-		
-		//Initialisation des autres parametres. 
+
+		// Initialisation des autres parametres.
 		SystemRequests.getSettings(this);
 	}
 
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
-	
 	/**
-	 * Methode pour modifier les parametres de la video. 
+	 * Methode pour modifier les parametres de la video.
 	 * 
-	 * @param setting 		Le parametre a modifier.
-	 * @param newValue		La nouvelle valeur du parametre. 
+	 * @param setting  Le parametre a modifier.
+	 * @param newValue La nouvelle valeur du parametre.
 	 */
 	public void modifySettings(String setting, String newValue) {
 		oldSettings.put(setting, settings.get(setting));
 		settings.put(setting, newValue);
 	}
 
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
-	
-	
-	
 	/**
 	 * [ METHODE POUR SAVOIR SI DES PARAMETRES ONT ETE MODIFIES. ]
 	 * 
 	 * Methode pour tester si les parametres du fichier ont ete modifies.
 	 * 
-	 * @return booleen		True si le fichier a ete modifie. 
+	 * @return booleen True si le fichier a ete modifie.
 	 */
 	public boolean isModified() {
 		return !oldSettings.equals(new HashMap<String, Object>());
@@ -107,14 +82,10 @@ public class SettingsFile extends SelectableFile {
 	 * 
 	 * Recupere les parametres du fichiers.
 	 * 
-	 * @return HashMap<String, String>			Les parametres du fichier. 
+	 * @return HashMap<String, String> Les parametres du fichier.
 	 */
 	public HashMap<String, Object> getSettings() {
 		return settings;
 	}
 
-	
-	
-	//=======================================================================================================================
-	//=======================================================================================================================
 }

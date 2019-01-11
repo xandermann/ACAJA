24e0b1bd741f85ce512650891d7b7f2a38e01290
chevrier jc <chevrie44u@etu.univ-lorraine.fr>
2019-01-11 01:02:58 +0100

    Ajout de diagrammes uml.

--- src/gui_conversion/ConversionPanel.java
+++ src/gui_conversion/ConversionPanel.java
@@ -167,7 +167,7 @@ public class ConversionPanel extends JFrame{
 					JFrame chargement = new JFrame("Convertion de votre fichier");
 					OpeningWindow.afficherLogo(chargement);
 
-					chargement.setLayout(new FlowLayout());
+					chargement.setLayout(new BorderLayout());
 					chargement.setSize(400, 150);
 					JLabel c = new JLabel("Conversion de votre fichier ...");
 					chargement.add(c,BorderLayout.CENTER);
@@ -177,15 +177,6 @@ public class ConversionPanel extends JFrame{
 					chargement.setLocation(((int)ecran.getWidth()-400)/2, ((int)ecran.getHeight()-150)/2);
 					chargement.setVisible(true);					
 					
-					/*Thread t = new Thread();
-					while(model.convert()) {
-						c.setText("Convertion de votre fichier .");
-						t.sleep(20);
-						c.setText("Convertion de votre fichier ..");
-						t.sleep(20);
-						c.setText("Convertion de votre fichier ...");
-						t.sleep(20);
-					}*/
 					model.convert();
 					
 				    chargement.dispose();

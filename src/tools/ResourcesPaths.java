fca7f4173b92c7c6f86882a3a4289fb9752d25e0
chevrier jc <chevrie44u@etu.univ-lorraine.fr>
2019-01-28 10:13:08 +0100

    Correction.

--- src/tools/ResourcesPaths.java
+++ src/tools/ResourcesPaths.java
@@ -1,9 +1,5 @@
 package tools;
-
-import java.awt.Image;
-import java.io.File;
 import javax.imageio.ImageIO;
-
 public final class ResourcesPaths {
 	public final static String ACAJA_LOGO_PATH = "img/LogoAcaja.png";
 }

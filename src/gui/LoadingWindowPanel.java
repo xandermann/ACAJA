aee7f2f9e276562bd1be54b67b02e55a132b0168
xandermann <contact@alexandre-hublau.com>
2019-02-08 17:26:38 +0100

    Ajout commentaires

--- src/gui/LoadingWindowPanel.java
+++ src/gui/LoadingWindowPanel.java
@@ -6,21 +6,35 @@ import javax.swing.JPanel;
 
 import style.Style;
 
+/**
+ * Classe qui genere le panel de chargement de l'application
+ *
+ */
 public class LoadingWindowPanel extends JPanel {
 
+	/**
+	 * Pourcentage de la barre de chargement
+	 */
 	private int percentCurrentProgress;
-	
+
+	/**
+	 * Affiche les elements a l'interieur de la fenetre
+	 */
 	public void paintComponent(Graphics g) {
-		
-	
+
 		g.setColor(Style.BACKGROUND_SECONDARY);
 		g.fillRect(20, 20, 200, 20);
-		
+
 		g.setColor(Style.BACKGROUND_LIGHT);
-		g.fillRect(20, 20, (this.percentCurrentProgress*200)/100, 20);
-		
+		g.fillRect(20, 20, (this.percentCurrentProgress * 200) / 100, 20);
+
 	}
 
+	/**
+	 * Met a jour la progression de la barre de chargement contenue dans ce JPanel
+	 * 
+	 * @param progress
+	 */
 	public void setProgress(int progress) {
 		this.percentCurrentProgress = progress;
 		this.repaint();

b1340bc7d2559fa8e08ca349bab338535a2f1e3a
dasilvac9u <alexandre.da-silva-carmo9@etu.univ-lorraine.fr>
2019-02-18 12:24:22 +0100

    visu traitement

--- src/gui/traitement/PanelImage.java
+++ src/gui/traitement/PanelImage.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -20,6 +21,8 @@ public class PanelImage extends JPanel{
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		ToolTraitement.drawDeco(g, this.size().height, this.size().width);
-		
+		ImageIcon m = new ImageIcon(tools.ResourceConstants.ACAJA_LOGO_PATH_Opacity);
+        Image monImage = m.getImage();
+        g.drawImage(monImage, this.size().height/2-130, this.size().width/2-150,this);
 	}
 }

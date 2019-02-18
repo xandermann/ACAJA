8fa2101e2fe794f72ff546231af68c265a6e2c12
Adrien Palmieri <adrien.palmieri9@etu.univ-lorraine.fr>
2019-02-18 17:52:22 +0100

    ajout debut fonctionnalite historique

---
+++ src/gui/conversion/FileInformation.java
@@ -0,0 +1,21 @@
+package gui.conversion;
+
+import java.io.Serializable;
+
+public class FileInformation implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String fileName;
+	private String path;
+	public FileInformation(String p_name, String p_path) {
+		this.fileName = p_name;
+		this.path = p_path;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public String getPath() {
+		return path;
+	}
+
+}

33040b08ab5df484d8796ced5ed1d706c727b43c
xandermann <contact@alexandre-hublau.com>
2019-02-26 14:54:45 +0100

    Modification mise en page des classes pour augmenter la lisibilité

--- src/gui/style/StylizedJMenu.java
+++ src/gui/style/StylizedJMenu.java
@@ -1,11 +1,7 @@
 package gui.style;
 
 
-import java.awt.Graphics;
-import java.awt.Insets;
-
 import javax.swing.JMenu;
-import javax.swing.border.Border;
 
 public class StylizedJMenu extends JMenu implements StylizedComponent {
 

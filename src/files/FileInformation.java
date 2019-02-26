6047ff6aca0ea5d652b0e10a9297aa9480147d48
xandermann <contact@alexandre-hublau.com>
2019-02-26 14:40:31 +0100

    Optimisations

--- src/files/FileInformation.java
+++ src/files/FileInformation.java
@@ -7,13 +7,16 @@ public class FileInformation implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String fileName;
 	private String path;
+
 	public FileInformation(String p_name, String p_path) {
 		this.fileName = p_name;
 		this.path = p_path;
 	}
+
 	public String getFileName() {
 		return fileName;
 	}
+
 	public String getPath() {
 		return path;
 	}

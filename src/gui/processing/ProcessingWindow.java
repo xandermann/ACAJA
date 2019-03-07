79ab66528edd3cbb98bc4fcd3dd9729cb0411b98
dasilvac9u <alexandre.da-silva-carmo9@etu.univ-lorraine.fr>
2019-03-07 10:30:55 +0100

    rajout JMenu

--- src/gui/processing/ProcessingWindow.java
+++ src/gui/processing/ProcessingWindow.java
@@ -3,6 +3,8 @@ package gui.processing;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -16,8 +18,10 @@ import exceptions.UnfindableResourceException;
 import files.files.SelectableFile;
 import gui.JFileChooserManager;
 import gui.WindowTools;
+import gui.conversion.ConversionWindow;
 import gui.style.StylizedJMenuBar;
 import gui.style.StylizedJMenuItem;
+import resources.ResourcesManager;
 import wrapper.runtime.global.SystemRequests;
 
 public class ProcessingWindow extends JFrame {
@@ -27,6 +31,17 @@ public class ProcessingWindow extends JFrame {
 	
 	private ProcessingWindow() {
 		
+		this.addWindowListener(new WindowListener() {
+			public void windowOpened(WindowEvent e) {}
+			public void windowClosing(WindowEvent e) {
+				ResourcesManager.clearResources();
+			}
+			public void windowClosed(WindowEvent e) {}
+			public void windowIconified(WindowEvent e) {}
+			public void windowDeiconified(WindowEvent e) {}
+			public void windowActivated(WindowEvent e) {}
+			public void windowDeactivated(WindowEvent e) {}
+		});
 		this.model = new ModelARenomer();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.createJMenu();
@@ -42,6 +57,7 @@ public class ProcessingWindow extends JFrame {
 		this.setResizable(false);
 		this.add(p);
 		
+		
 	}
 
 	
@@ -51,9 +67,26 @@ public class ProcessingWindow extends JFrame {
 		jm.add(fileMenu);
 		JMenu libraryMenu = new JMenu("Bibliotheque");
 		jm.add(libraryMenu);
+		/*
 		JMenu videoMenu = new JMenu("Video");
-		jm.add(videoMenu);
+		jm.add(videoMenu);*/
 		
+		StylizedJMenuItem procToConv = new StylizedJMenuItem("Passer en mode conversion");
+		procToConv.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ConversionWindow.generateConversionWindow();
+				dispose();
+			}
+		});
+		StylizedJMenuItem convertir = new StylizedJMenuItem("Traiter");
+		StylizedJMenuItem quit = new StylizedJMenuItem("Quitter");
+		quit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
 		StylizedJMenuItem importFile = new StylizedJMenuItem("Importer un fichier");
 		importFile.addActionListener(new ActionListener() {
 			@Override
@@ -73,6 +106,10 @@ public class ProcessingWindow extends JFrame {
 		});
 		
 		libraryMenu.add(importFile);
+		
+		fileMenu.add(procToConv);
+		fileMenu.add(convertir);
+		fileMenu.add(quit);
 
 		this.setJMenuBar(jm);
 

3344c9586043feabd14483a617426199294ccd9e
chevrier jc <chevrie44u@etu.univ-lorraine.fr>
2019-03-11 13:29:21 +0100

    Ajout des flags pour l'ajout et la suppression de bande sons.

--- src/wrapper/language/FlagConstants.java
+++ src/wrapper/language/FlagConstants.java
@@ -15,28 +15,45 @@ public interface FlagConstants {
 	public final static String FLAG_SEPARATOR = ":";
 	public final static String S = FLAG_SEPARATOR;
 	
+	public final static String FLAG_MAP = "-map";
+	
+	public final static String FLAG_FILTER = "-c";
+	public final static String FLAG_FILTER_AUDIO = FLAG_FILTER+S+"v";
+	public final static String FLAG_FILTER_VIDEO = FLAG_FILTER+S+"a";
+	public final static String FLAG_COPY = "copy";
+	public final static String FLAG_FILTER_ALL_COPY = FLAG_FILTER+S+FLAG_COPY;
+	
 	public final static String FLAG_SUPPORTED_CODECS = "-codecs";
 	
 	public final static String FLAG_QUALITY = "-crf";
+	public final static String FLAG_SAME_QUALITY = "-qscale";
 	
 	public final static String FLAG_VIDEO_CODEC = "-vcodec";
-	public final static String FLAG_VIDEO_BITRATE = "-b:v";
+	public final static String FLAG_VIDEO_BITRATE = "-b"+S+"v";
 	public final static String FLAG_FRAMERATE = "-r";
 	public final static String FLAG_RESOLUTION = "-s";
 	
 	public final static String FLAG_AUDIO_CODEC = "-acodec";
-	public final static String FLAG_AUDIO_BITRATE = "-b:a";
+	public final static String FLAG_AUDIO_BITRATE = "-b"+S+"a";
 	public final static String FLAG_SAMPLING_RATE =  "-ar";
 	public final static String FLAG_NUMBER_AUDIO_CHANNELS = "-ac";
 	
-	public final static String[] FLAG_RESIZE = {"-vf", "scale="};
+	public final static String FLAG_MODIFY  = "-vf";
+	
+	public final static String[] FLAG_RESIZE = {FLAG_MODIFY, "scale="};
 	public final static String[] FLAG_CROP = {"-filter:v", "\"crop=", "\""};
 	public final static String[] FLAG_PERIOD = {"-ss", "-t"};
-	public final static String[] FLAG_ROTATE = {"-vf", "transpose=1"};
+	public final static String[] FLAG_ROTATE = {FLAG_MODIFY, "\"transpose=1\""};
 	public final static String[] FLAG_BLUR = {"-filter_complex", 
 											  "\"[0:v]crop=", ",boxblur=10[fg];[0:v][fg]overlay=", "[v]\"",
-										      "-map", "\"[v]\"", "-map", "0:a", 
-										      "-c:v", "libx264", "-c:a", "copy", "-movflags", "+faststart"};
+										      "-map", "\"[v]\"", "-map", "0"+S+"a", 
+										      FLAG_FILTER_VIDEO, "libx264", FLAG_FILTER_AUDIO, FLAG_COPY, 
+										      "-movflags", "+faststart"};
 	
-	public final static String[] FLAG_ADD = {"-f", "concat", FLAG_INPUT, "-c:copy"};
+	public final static String[] FLAG_ADD = {"-f", "concat", FLAG_INPUT, FLAG_FILTER_ALL_COPY};
+	
+	public final static String[] FLAG_REMOVE_SOUND = {FLAG_FILTER, FLAG_COPY, "-an"};
+	
+	public final static String[] FLAG_ADD_SOUND = {FLAG_INPUT, FLAG_FILTER, FLAG_COPY, FLAG_MAP, "0"+S+"0", FLAG_MAP, "0"+S+"1"};
+
 }

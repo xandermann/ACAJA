eedab532ab6f35a84deba02d749891e1397d1fd1
chevrier jc <chevrie44u@etu.univ-lorraine.fr>
2019-03-22 20:26:16 +0100

    Mise a jour.

--- src/gui/conversion/views/TwoTextsView.java
+++ src/gui/conversion/views/TwoTextsView.java
@@ -10,27 +10,27 @@ public final class TwoTextsView extends JLabel {
 	/**
 	 * [ TEXTES INJECTES. ]
 	 */
-	private String first, two;
+	private String first, second;
 	/**
 	 * [ POLICES DES TEXTES. ]
 	 */
-	private int sizeFirst, sizeTwo;
+	private int sizeFirst, sizeSecond;
 	
 	
 	
 	/**
 	 *  [ CONSTRUIT UN TEXTE PROPRE ET FORMATE. ]
 	 *  
-	 * @param first				Premier texte a injecter. 
-	 * @param sizeFirst			Police du premier texte (en px). 
-	 * @param two				Second texte a injecter. 
-	 * @param sizeTwo			Police du second texte (en px). 
+	 * @param first					Premier texte a injecter. 
+	 * @param sizeFirst				Police du premier texte (en px). 
+	 * @param second				Second texte a injecter. 
+	 * @param sizeSecond			Police du second texte (en px). 
 	 */
-	public TwoTextsView(String first, int sizeFirst, String two, int sizeTwo) {
+	public TwoTextsView(String first, int sizeFirst, String second, int sizeSecond) {
 		if((this.first=first)==null) throw new NullPointerException("Premier texte null !");
-		if((this.two=two)==null) throw new NullPointerException("Second texte null !");
+		if((this.second=second)==null) throw new NullPointerException("Second texte null !");
 		if((this.sizeFirst=sizeFirst)<=0) throw new IllegalArgumentException("Police incorrecte !");
-		if((this.sizeTwo=sizeTwo)<=0) throw new IllegalArgumentException("Police incorrecte  !");
+		if((this.sizeSecond=sizeSecond)<=0) throw new IllegalArgumentException("Police incorrecte  !");
 		
 		setText(text());
 		setHorizontalAlignment(JLabel.CENTER);
@@ -53,9 +53,9 @@ public final class TwoTextsView extends JLabel {
 							"#first {" +
 								"font-size: "+sizeFirst+"px;" +
 							"}" +
-							"#two {" +
+							"#second {" +
 								"color: #0000FF;" +
-								"font-size: "+sizeTwo+"px;" +
+								"font-size: "+sizeSecond+"px;" +
 							"}" +
 						"</style>" +
 				   "</head>" +
@@ -65,8 +65,8 @@ public final class TwoTextsView extends JLabel {
 				   				first +
 				   			"</p>" +
 				   			"<br>"+
-				   			"<p id=two>" + 
-				   				two.toUpperCase() +
+				   			"<p id=second>" + 
+				   				second.toUpperCase() +
 				   			"</p>" + 
 				   	   "</div>"+
 				  "</body>" + 
@@ -78,10 +78,10 @@ public final class TwoTextsView extends JLabel {
 	/**
 	 * [ CHANGER LE CONTENU DU DEUXIEME TEXTE. ]
 	 * 
-	 * @param two			  Second texte a injecter. 
+	 * @param second			  Second texte a injecter. 
 	 */
-	public void $(String two) {
-		if((this.two=two)==null) throw new NullPointerException("Second texte null !");
+	public void $(String second) {
+		if((this.second=second)==null) throw new NullPointerException("Second texte null !");
 		setText(text());
 	}
 }

873fed683fa112bc91bf61bfda67b9a1e193d130
xandermann <contact@alexandre-hublau.com>
2019-04-03 13:10:30 +0200

    Ajout tests fichier

--- src/threads/ThreadForSave.java
+++ src/threads/ThreadForSave.java
@@ -8,45 +8,44 @@ import wrapper.runtime.global.UserRequests;
 /**
  * [ CLASSE THREAD POUR LANCER LA SAUVEGARDE VIA FFMPEG. ]
  * 
- * Auteurs du projet : 
- * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et CHEVRIER Jean-christophe.
+ * Auteurs du projet :
+ * 
+ * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et
+ *         CHEVRIER Jean-christophe.
  */
-public final class ThreadForSave extends Thread{
+public final class ThreadForSave extends Thread {
 	/**
 	 * [ ATTRIBUT D'INSTANCE. ]
 	 */
 	private SelectableFile file;
-	
-	
+
 	/**
 	 * [ CONSTRUCTEUR INTERNE - ENCAPSULE ]
 	 * 
-	 * @param file		Le fichier a traiter. 
+	 * @param file Le fichier a traiter.
 	 */
 	private ThreadForSave(SelectableFile file) {
-		if((this.file = file) == null)
+		if ((this.file = file) == null)
 			throw new NullPointerException("Le file recu en parametre est null !");
 	}
-	
-	
+
 	/**
 	 * [ DEROULEMENT DU THREAD - SAUVEGARDE. ]
 	 */
-    public void run() {
-    	try {
-    		UserRequests.execute(file);
+	public void run() {
+		try {
+			UserRequests.execute(file);
 		} catch (UnfindableResourceException ure) {
 			JOptionPane.showMessageDialog(null, ure.getMessage());
 		}
-    }
-    
-    
-    /**
-     * [ METHODE POUR LANCER LE THREAD DE LA SAUVEGARDE VIA FFMPEG. ]
-     * 
-     * @param file		Le fichier a traiter.  
-     */
-    public static void saveInNewThread(SelectableFile file) {
-    	(new ThreadForSave(file)).start();
-    }
+	}
+
+	/**
+	 * [ METHODE POUR LANCER LE THREAD DE LA SAUVEGARDE VIA FFMPEG. ]
+	 * 
+	 * @param file Le fichier a traiter.
+	 */
+	public static void saveInNewThread(SelectableFile file) {
+		(new ThreadForSave(file)).start();
+	}
 }

c548876c94d8adb040d233406ed98408514ff4d5
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2019-04-04 18:21:25 +0200

    fix

--- src/gui/general/GeneralWindow.java
+++ src/gui/general/GeneralWindow.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 import javax.swing.*;
 import gui.WindowTools;

91b1ce9f57b482000f99f671e050f3fca563c5b2
xandermann <contact@alexandre-hublau.com>
2019-04-04 18:32:14 +0200

    Alert

--- src/gui/alerts/Alert.java
+++ src/gui/alerts/Alert.java
@@ -1,88 +1,97 @@
 package gui.alerts;
-import java.awt.event.*;
-import javax.swing.*;
 
-import gui.general.Context;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
 /**
  * [ ALERTES ( = NOTIFICATIONS TEMPORAIRES ). ]
  * 
- * Auteurs du projet : 
- * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et CHEVRIER Jean-christophe.
+ * Auteurs du projet :
+ * 
+ * @author HUBLAU Alexandre, PAMIERI Adrien, DA SILVA CARMO Alexandre, et
+ *         CHEVRIER Jean-christophe.
  */
-public final class Alert extends AlertSettings implements ATConstants, AMConstants{
+public final class Alert extends AlertSettings implements ATConstants, AMConstants {
 	/**
 	 * [ VUE DE L'ALERTE. ]
 	 */
 	private AlertWindow view;
-	
-	
+
 	/**
 	 * [ CONSTRUIT UNE ALERTE. ]
 	 * 
-	 * @param title				Titre / resume de la notification.
-	 * @param content			Details de la notification.
+	 * @param title   Titre / resume de la notification.
+	 * @param content Details de la notification.
 	 */
 	public Alert(String title, String content) {
-		if(INTERRUPTOR) 
+		if (INTERRUPTOR)
 			view = new AlertWindow(title, content);
-		
+
 		view.addMouseListener(new MouseListener() {
-			public void mouseClicked(MouseEvent e) {}
-			public void mousePressed(MouseEvent e) {}
-			public void mouseReleased(MouseEvent e) {
-				view.dispose();				
+			public void mouseClicked(MouseEvent e) {
 			}
-			public void mouseEntered(MouseEvent e) {}
-			public void mouseExited(MouseEvent e) {}
+
+			public void mousePressed(MouseEvent e) {
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				view.dispose();
+			}
+
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			public void mouseExited(MouseEvent e) {
+			}
 		});
 	}
-	
-	
-	
+
 	/**
 	 * [ ALERTER L'UTILISATEUR PENDANT UN TEMPS FIXE. ]
 	 * 
-	 * @param timeOut		Temps d'affichage de la notification.
+	 * @param timeOut Temps d'affichage de la notification.
 	 */
 	public void alert(long timeOut) {
-		if(INTERRUPTOR) {
-			if(timeOut<0) 
+		if (INTERRUPTOR) {
+			if (timeOut < 0)
 				throw new IllegalArgumentException("timeOut negatif !");
-			if(timeOut != 0) {
+			if (timeOut != 0) {
 				SwingUtilities.invokeLater(new Runnable() {
-				    public void run() {
-				    	view.setVisible(true);
-				    	new Thread() {
-				    		 public void run() {
-				 				try {
-				 					while(!view.isVisible()) Thread.yield();
+					public void run() {
+						view.setVisible(true);
+						new Thread() {
+							public void run() {
+								try {
+									while (!view.isVisible())
+										Thread.yield();
 									Thread.sleep(timeOut);
 								} catch (InterruptedException ie) {
 									JOptionPane.showMessageDialog(null, ie.getMessage());
 								}
-				 				view.dispose();
-				    		 }
-				    	}.start();
-				    }
+								view.dispose();
+							}
+						}.start();
+					}
 				});
 			}
 		}
 	}
-	
-	
+
 	/**
 	 * [ ALERTER L'UTILISATEUR PENDANT UN TEMPS. ]
 	 * 
-	 * @param title				Titre / resume de la notification.
-	 * @param content			Details de la notification.
+	 * @param title   Titre / resume de la notification.
+	 * @param content Details de la notification.
 	 * 
 	 * @see #alert
 	 */
 	public static void alert(String title, String content, int timeOut) {
 		new Alert(title, content).alert(timeOut);
 	}
-	
-	
+
 	/**
 	 * [ ALERTER L'UTILISATEUR PENDANT "SHORT" SECONDES. ]
 	 * 
@@ -92,21 +101,19 @@ public final class Alert extends AlertSettings implements ATConstants, AMConstan
 	public void shortAlert() {
 		alert(SHORT);
 	}
-	
-	
+
 	/**
 	 * [ ALERTER L'UTILISATEUR PENDANT "SHORT" SECONDES. ]
 	 * 
-	 * @param title				Titre / resume de la notification.
-	 * @param content			Details de la notification.
+	 * @param title   Titre / resume de la notification.
+	 * @param content Details de la notification.
 	 * 
 	 * @see #shortAlert
 	 */
 	public static void shortAlert(String title, String content) {
 		new Alert(title, content).shortAlert();
 	}
-	
-	
+
 	/**
 	 * [ ALERTER L'UTILISATEUR PENDANT "LONG" SECONDES. ]
 	 * 
@@ -116,13 +123,12 @@ public final class Alert extends AlertSettings implements ATConstants, AMConstan
 	public void longAlert() {
 		alert(LONG);
 	}
-	
-	
+
 	/**
 	 * [ ALERTER L'UTILISATEUR PENDANT "LONG" SECONDES. ]
 	 * 
-	 * @param title				Titre / resume de la notification.
-	 * @param content			Details de la notification.
+	 * @param title   Titre / resume de la notification.
+	 * @param content Details de la notification.
 	 * 
 	 * @see #longAlert
 	 */
